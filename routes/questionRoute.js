4d39b6e4e1334d571e8e03823740a7b5f8ebf218
ppcheng <ppcheng@uwaterloo.ca>
2015-10-14 18:26:07 -0400

    Use post over put for the status change api

--- routes/questionRoute.js
+++ routes/questionRoute.js
@@ -1,12 +1,12 @@
 var QuestionCtrl = require('../controllers/questionCtrl.js');
 
-module.exports = function(express, app){
+module.exports = function(express, app) {
   var router = express.Router();
 
   router.post('/', QuestionCtrl.create);
   router.get('/', QuestionCtrl.get);
 
-  router.put('/status', QuestionCtrl.set);
+  router.post('/status', QuestionCtrl.set);
 
   app.use('/questions', router);
 };
